b77d629b2c14fbd9679f5d4eafab627f1659c32a
Huan Xu <huanxu@Huans-MacBook-Air.local>
2022-01-23 14:00:37 -0600

    Updates

---
+++ precache-manifest.563fd8840720b7f23682fcca944af2fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbab63ebcf801180855e4afd32e7cc7b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e040cfdf447739f80ee1",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e040cfdf447739f80ee1",
+    "url": "/home/static/js/main.43431371.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "bc2eedcab0589ce55ecdbccf1883fd81",
+    "url": "/home/static/media/huan.bc2eedca.png"
+  },
+  {
+    "revision": "172dd81eb5fb6f064448568b5131666f",
+    "url": "/home/static/media/resume.172dd81e.pdf"
+  }
+]);
